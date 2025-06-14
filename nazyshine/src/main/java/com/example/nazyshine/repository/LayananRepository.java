0f67343a5a2f2690388d83d0695abc7f22f5b37c
cipacips <shifadondoo@gmail.com>
2025-06-14 10:41:56 +0700

    e

--- nazyshine/src/main/java/com/example/nazyshine/repository/LayananRepository.java
+++
@@ -1,9 +0,0 @@
-// src/main/java/com/example/nazyshine/repository/LayananRepository.java
-package com.example.nazyshine.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.nazyshine.model.Layanan;
-
-public interface LayananRepository extends JpaRepository<Layanan, Long> { // ID Layanan adalah Long, konsisten
-}
